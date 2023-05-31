c8302d8ee3b1b1635e25b38fd9b9a9f540877eb8
Marquise Campbell <marquisetyrrell@gmail.com>
2023-05-31 10:58:46 -0400

    second

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿// Find the words in the collection that start with the letter 'L'
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+// Find the words in the collection that start with the letter 'L'
 List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
 
 IEnumerable<string> LFruits = fruits.Where(fruit => fruit.StartsWith("L"));
@@ -6,4 +11,13 @@ foreach (string fruit in LFruits){
     Console.WriteLine($"{fruit}");
 }
                             
-
+// Which of the following numbers are multiples of 4 or 6
+List<int> numbers = new List<int>()
+{
+    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+};
+// || is the or operator couldn't remember that. 
+IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0 );
+foreach (int number in fourSixMultiples){
+    Console.WriteLine($"{number}");
+}
