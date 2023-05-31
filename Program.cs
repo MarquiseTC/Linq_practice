5a5a79b2830d7d4217d385a50c7ae1704b279168
Marquise Campbell <marquisetyrrell@gmail.com>
2023-05-31 13:37:13 -0400

    lost count

--- Program.cs
+++ Program.cs
@@ -47,3 +47,28 @@ List<int> ascend = numbs.OrderBy(numb => numb).ToList();
 foreach ( int numb in ascend){
     Console.WriteLine($"{numb}");
 }
+
+// Output how many numbers are in this list
+List<int> numers = new List<int>()
+{
+    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+};
+// .Count is counting number of elements in List
+Console.WriteLine(numers.Count());
+
+// How much money have we made?
+List<double> purchases = new List<double>()
+{
+    2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+};
+double total = purchases.Sum();
+Console.WriteLine(Math.Round(total)); 
+// Rounding here to get an even number
+
+// What is our most expensive product?
+List<double> prices = new List<double>()
+{
+    879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+};
+ double largest = prices.Max();
+ Console.WriteLine("Our most expensive item is" + largest);
